810bddd220a7fb4613077d7b613660c249e85720
Ivan Budanov <imb@xrs.ru>
2024-05-02 16:35:40 +0300

    update data

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -22,6 +22,7 @@ while (true)
 
     if (key == "1")
     {
+        Console.Clear();
         var serial = new Serial();
         Console.Write("Send title: ");
         var title = Console.ReadLine();
@@ -45,30 +46,86 @@ while (true)
         serials.Add(serial);
 
         saver.Save(filePath, serials);
+        Console.Clear();
     }
     else if (key == "2")
     {
-        foreach (var serial in serials.OrderByDescending(x => x.WatchPercent))
+        bool exit = false;
+        var page = 1;
+        bool canNext = false;
+        bool canBack = false;
+        while (!exit)
         {
-            serial.WatchPercent = calc.GetPercent(serial.ActualSeries, serial.MaxSeries);
-        }
-        saver.Save(filePath, serials);
+            
+            Console.Clear();
+            foreach (var serial in serials.OrderByDescending(x => x.WatchPercent))
+            {
+                serial.WatchPercent = calc.GetPercent(serial.ActualSeries, serial.MaxSeries);
+            }
 
-        Console.WriteLine();
-        
-        var serialsOrdered = serials.Where(x => x.WatchPercent < 100.0).OrderByDescending(x => x.WatchPercent).ToList();
-        
-        for (int i = 0; i < serialsOrdered.Count; i++)
-        {
-            var time = (serialsOrdered[i].MaxSeries - serialsOrdered[i].ActualSeries) * 22;
-            Console.WriteLine($"{$"{i+1}.",-4} {serialsOrdered[i].Title,-75}| {serialsOrdered[i].ActualSeries,-3}/{serialsOrdered[i].MaxSeries,-3} | {$"{Math.Round(serialsOrdered[i].WatchPercent, 2)}%",-6} {time/60:00}:{time%60:00}");
+            saver.Save(filePath, serials);
+
+            Console.WriteLine();
+
+            var serialsOrdered = serials.Where(x => x.WatchPercent < 100.0).OrderByDescending(x => x.WatchPercent).ToList();
+
+            var maxOnPage = 10;
+            var max = page * maxOnPage;
+            var see = maxOnPage;
+            if (serialsOrdered.Count < max) see = Math.Abs(max - serialsOrdered.Count);
+            var maxPage = serialsOrdered.Count / maxOnPage + (serialsOrdered.Count % maxOnPage > 0 ? 1 : 0);
+            if (page < maxPage) canNext = true;
+            if (page == maxPage) canNext = false;
+            if (page == 1) canBack = false;
+            if (page > 1) canBack = true;
+            
+            Console.WriteLine($"{page}/{maxPage}");
+            Console.WriteLine($"{"№",-4} {"Title",-75}| {"Series",-7} | {"Percent",-7} | HH:mm");
+
+            
+            for (int i = 0; i < see; i++)
+            {
+                var index = i + see * (page - 1);
+                var time = (serialsOrdered[index].MaxSeries - serialsOrdered[index].ActualSeries) * 22;
+                Console.WriteLine(
+                    $"{$"{i + 1}.",-4} {serialsOrdered[index].Title,-75}| {serialsOrdered[index].ActualSeries,3}/{serialsOrdered[index].MaxSeries,3} | {$"{Math.Round(serialsOrdered[index].WatchPercent, 2)}%",7} | {time / 60:00}:{time % 60:00}");
+            }
+
+            // foreach (var serial in serials.OrderByDescending(x => x.WatchPercent))
+            // {
+            //     Console.WriteLine($"{serial.Title,-75}| {serial.ActualSeries,-3}/{serial.MaxSeries,-3} | {Math.Round(serial.WatchPercent, 2)}%");
+            // }
+
+            Console.WriteLine("\"q\" for exit");
+            var value = Console.ReadLine();
+            if (value == "q")
+            {
+                exit = true;
+            }
+            else if (value == "n" && canNext)
+            {
+                page++;
+                
+            }
+            else if (value == "b" && canBack)
+            {
+                page--;
+            }
+            else
+            {
+                if (int.TryParse(value, out var number))
+                {
+                    var editIndex = serials.FindIndex(x => x.Title == serialsOrdered[number-1].Title);
+                    if (serials[editIndex].ActualSeries + 1 > serials[editIndex].MaxSeries)
+                    {
+                    }
+                    else
+                    {
+                        serials[editIndex].ActualSeries+=1;
+                        saver.Save(filePath, serials);
+                    }
+                }
+            }
         }
-        
-        // foreach (var serial in serials.OrderByDescending(x => x.WatchPercent))
-        // {
-        //     Console.WriteLine($"{serial.Title,-75}| {serial.ActualSeries,-3}/{serial.MaxSeries,-3} | {Math.Round(serial.WatchPercent, 2)}%");
-        // }
-        
-        Console.WriteLine();
     }
 }
